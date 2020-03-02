80aee263ca674b75f130b382ca4084ddb109e83f
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2020-03-02 13:52:09 +0500

    add test for different filters, occlusions and difficulties

--- Test/recognition.cpp
+++ Test/recognition.cpp
@@ -14,7 +14,12 @@ using namespace Faces::Datasets;
 const std::string data_dir = "../Test/";
 const std::string config_file = "../config4test.json";
 int num_train_samples = 7;
-int max_persons = 45;
+int max_persons = 50;
+std::string postfix_str;
+
+Occlusion_SoF occlusion = Occlusion_SoF::EYE;
+Image_filters_SoF filters = Image_filters_SoF::NORMAL;
+Difficulty_SoF difficulty = Difficulty_SoF::ORIGINAL;
 
 template<typename T, typename T_a>
 void get_ids(T *dataset, std::vector<T_a> &annotations) {
@@ -94,9 +99,7 @@ std::tuple<int, int> test(T *dataset, std::vector<T_a> &annotations, Faces::Face
             faces.detector.faces.clear();
 
             auto annotation_obj = annotation.annotations.back();
-            cv::Mat sample = annotation.images_loader.load(Occlusion_SoF::EYE,
-                                                           Image_filters_SoF::NORMAL,
-                                                           Difficulty_SoF::ORIGINAL);
+            cv::Mat sample = annotation.images_loader.load(occlusion, filters, difficulty);
 
             Faces::Face face(annotation_obj.rect.tl(), annotation_obj.rect.br(), faces.recognition.currentLabel);
             faces.detector.faces.emplace_back(face);
@@ -114,9 +117,9 @@ std::tuple<int, int> test(T *dataset, std::vector<T_a> &annotations, Faces::Face
                 failed++;
                 annotation_obj.id += ", not " + faces.recognition.labels[predicted_id];
                 annotation_obj.draw(sample, cv::Scalar(0, 255, 0));
-                std::string path = data_dir + "/recognition_fails/" + std::to_string(real_label_id) + "_" +
+                std::string path = data_dir + "/recognition_fails_2/" + std::to_string(real_label_id) + "_" +
                                    std::to_string(predicted_id) + "_" + std::to_string(failed);
-                path += "_" + std::to_string(num_train_samples) + "_" + std::to_string(max_persons) + ".jpg";
+                path += "_" + postfix_str + ".jpg";
                 cv::imwrite(path, sample);
             }
         }
@@ -125,30 +128,10 @@ std::tuple<int, int> test(T *dataset, std::vector<T_a> &annotations, Faces::Face
     return std::make_tuple(successfully, failed);
 }
 
-int main(int argc, char **argv) {
-    Faces::Faces faces(config_file);
-    faces.detectFreq = 0;
-    faces.recognition.minLabelNotChanged = 0;
-    faces.recognizeFreq = 0;
-
-    int successfully = 0;
-    int failed = 0;
-
-    auto *dataset = ::Dataset_SoF::create(
-            "/home/prostoichelovek/Documents/datasets/faces/sof/metadata/metadata.json",
-            "/home/prostoichelovek/Documents/datasets/faces/sof/images");
-
-    std::vector<Annotation_SoF> annotations;
-    get_ids(dataset, annotations);
-    if (argc > 1) {
-        system("rm ../data/samples_test -r");
-        system("rm ../data/labels_test.txt -r");
-        add_samples(dataset, faces, annotations);
-    }
-
+template<typename T, typename T_a>
+void test_numbers(T *dataset, std::vector<T_a> &annotations, Faces::Faces &faces) {
     std::ofstream log_fs("../Test/log.txt", std::ios::app);
     log_fs << "==========================================" << std::endl;
-
     int best_num_persons = max_persons;
     int best_success_percent = 0;
     for (; num_train_samples < 20; num_train_samples += 4) {
@@ -156,6 +139,7 @@ int main(int argc, char **argv) {
             faces.recognition.train(faces.cfg.samples_directory, max_persons, num_train_samples);
             faces.recognition.save(faces.cfg.recognition_classifier);
 
+            int successfully = 0, failed = 0;
             std::tie(successfully, failed) = test(dataset, annotations, faces);
             int success_percent = ((float) successfully / (float) (successfully + failed)) * 100;
             if (success_percent > best_success_percent) {
@@ -177,6 +161,94 @@ int main(int argc, char **argv) {
         }
     }
 
+}
+
+int main(int argc, char **argv) {
+    Faces::Faces faces(config_file);
+    faces.detectFreq = 0;
+    faces.recognition.minLabelNotChanged = 0;
+    faces.recognizeFreq = 0;
+
+    auto *dataset = ::Dataset_SoF::create(
+            "/home/prostoichelovek/Documents/datasets/faces/sof/metadata/metadata.json",
+            "/home/prostoichelovek/Documents/datasets/faces/sof/images");
+
+    std::vector<Annotation_SoF> annotations;
+    get_ids(dataset, annotations);
+    if (argc > 1 && strcmp(argv[1], "a") == 0) {
+        log(INFO, "Adding samples....");
+        system("rm ../data/samples_test -r");
+        system("rm ../data/labels_test.txt -r");
+        add_samples(dataset, faces, annotations);
+    } else {
+        for (auto &a : annotations) {
+            auto a_obj = a.annotations.back();
+            faces.recognition.addLabel(a_obj.id);
+        }
+    }
+
+    if (argc > 1 && strcmp(argv[1], "b") == 0) {
+        log(INFO, "Testing different numbers of persons and training samples...");
+        test_numbers(dataset, annotations, faces);
+    }
+
+    if (argc > 1 && strcmp(argv[1], "c") == 0) {
+        faces.recognition.train(faces.cfg.samples_directory, max_persons, num_train_samples);
+
+        log(INFO, "Testing different difficulties...");
+        std::ofstream log_fs("../Test/log2.txt", std::ios::app);
+        log_fs << "==========================================" << std::endl;
+        std::vector<std::pair<Occlusion_SoF, std::vector<std::pair<std::vector<Image_filters_SoF>, std::vector<Difficulty_SoF>>>>> params{
+                {Occlusion_SoF::EYE,           {
+                                                       {{Image_filters_SoF::NORMAL}, {Difficulty_SoF::ORIGINAL}},
+                                                       {{Image_filters_SoF::NOISE, Image_filters_SoF::SMOOTH, Image_filters_SoF::POSTERIZE},
+                                                               {Difficulty_SoF::EASY, Difficulty_SoF::MEDIUM, Difficulty_SoF::HARD}}
+                                               }},
+                {Occlusion_SoF::EYE_AND_NOSE,  {
+                                                       {{Image_filters_SoF::NORMAL}, {Difficulty_SoF::EASY, Difficulty_SoF::MEDIUM, Difficulty_SoF::HARD}}
+                                               }},
+                {Occlusion_SoF::EYE_AND_MOUTH, {
+                                                       {{Image_filters_SoF::NORMAL}, {Difficulty_SoF::EASY, Difficulty_SoF::MEDIUM, Difficulty_SoF::HARD}}
+                                               }}
+        };
+        for (int i = 0; i < params.size(); ++i) {
+            const auto &occlision_params = params[i];
+            occlusion = occlision_params.first;
+            for (int j = 1; j < occlision_params.second.size(); j++) {
+                const auto &filter_difficulty = occlision_params.second[j];
+                for (int k = 0; k < filter_difficulty.first.size(); k++) {
+                    filters = filter_difficulty.first[k];
+                    for (int l = 0; l < filter_difficulty.second.size(); l++) {
+                        difficulty = filter_difficulty.second[l];
+
+                        nlohmann::json tmp_json;
+                        postfix_str.clear();
+                        to_json(tmp_json, occlusion);
+                        postfix_str += std::string(tmp_json) + "_";
+                        to_json(tmp_json, filters);
+                        postfix_str += std::string(tmp_json) + "_";
+                        to_json(tmp_json, difficulty);
+                        postfix_str += std::string(tmp_json);
+
+                        int successfully = 0, failed = 0;
+                        std::tie(successfully, failed) = test(dataset, annotations, faces);
+                        int success_percent = ((float) successfully / (float) (successfully + failed)) * 100;
+
+                        log(INFO, "Postfix:", postfix_str);
+                        log(INFO, "Recognized successfully:", successfully);
+                        log(INFO, "Recognized wrong:", failed);
+                        log(INFO, "Success percent:", success_percent);
+                        log(INFO, "----------------");
+                        log_fs << "Postfix: " << postfix_str << std::endl;
+                        log_fs << "Recognized successfully: " << successfully << std::endl;
+                        log_fs << "Recognized wrong: " << failed << std::endl;
+                        log_fs << "Success percent: " << success_percent << std::endl;
+                        log_fs << "-----------------" << std::endl;
+                    }
+                }
+            }
+        }
+    }
 
     return EXIT_SUCCESS;
 }
