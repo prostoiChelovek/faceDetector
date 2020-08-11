ef2b64a8725e99a5d3b82e5838d9d80636008ddb
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2020-08-11 20:25:20 +0500

    document a Config file

--- src/Config/Config.h
+++ src/Config/Config.h
@@ -4,7 +4,8 @@
  * @date 09 Aug 2020
  * @copyright MIT License
  *
- * @brief
+ * @brief This file contains a wrapper around the config manager
+ *        and some helper macros to easily add new options to the config
  */
 
 #ifndef FACES_CONFIG_H
@@ -17,13 +18,29 @@
 
 #include <utils/utils.h>
 
-// TODO: add documentation
 
 namespace faces {
 
+/**
+ * Adds a new option to the config config
+ *
+ * @param name              - name of the option
+ * @param shortFlag         - name of the command line flag (e.g. --test)
+ * @param defaultVal        - value, the option will have before the initialization from command line or file
+ * @param valueRequired     - flag which indicates whether the option should be initialized or not
+ * @param valueDescription  - human-readable description of the option
+ */
 #define FACES_ADD_CONFIG_OPTION(name, shortFlag, defaultVal, valueRequired, valueDescription) \
-        Config::getInstance().config.option(name).shortflag(shortFlag).defaultValue(defaultVal).required(valueRequired).description(valueDescription);
+        Config::getInstance().config.option(name).shortflag(shortFlag).defaultValue(defaultVal) \
+                                        .required(valueRequired).description(valueDescription);
 
+/**
+ * Adds new options to the config outside of a function
+ * It creates an inline function and assigns its return value to a global variable and thus calling it
+ *
+ * @param name  - unique postfix for function and variable
+ * @param ...   - list of \ref FACES_ADD_CONFIG_OPTION macro calls; basically, it is a function body
+ */
 #define FACES_AUGMENT_CONFIG(name, ...) \
         inline static int __config_augment_dummy_fn_ ## name() { \
             __VA_ARGS__ \
@@ -32,42 +49,74 @@ namespace faces {
         inline static int __config_augment_dummy_var_ ## name = __config_augment_dummy_fn_ ## name();
 
 
+    /**
+     * Singleton wrapper around the config manager
+     */
     class Config {
     public:
+        /// the actual config manager
         miniconf::Config config = miniconf::Config();
 
+        /**
+         * @return a static instance of this class, creating it of necessary
+         */
         static Config &getInstance() {
             static Config instance;
             return instance;
         }
 
-        /* Accesses the configuration value
+        /**
+         * Accesses the configuration value
+         * A proxy operator to simplify accessing to the actual config`s operator[]
          *
-         * If the configuration value does not exist, an empty Value object is returned.
+         * @param flag - name of the option
+         *
+         * @return a reference to the value of the option
+         *         OR an empty miniconf::Value object if the option with the given name does not exist
          */
         miniconf::Value &operator[](const std::string &flag) {
             return config[flag];
         }
 
-        /* Accesses the configuration value
+        /**
+         * Accesses the configuration value
+         * A proxy operator to simplify accessing to the actual config`s operator[]
          *
-         * If the configuration value does not exist, std::out_of_range exception is thrown
+         * @param flag - name of the option
+         *
+         * @throws std::out_of_range - if the option with the given name does not exist
+         *
+         * @return a value of the option
          */
         miniconf::Value const &operator[](const std::string &flag) const {
             return config[flag];
         }
 
+        /**
+         * A helper method to get a path to some data file
+         *
+         * @param name          - a name of the config option with the name of the file
+         * @param dataSubdir    - a subdirectory of the 'dataDirectory' where the data file is located
+         *
+         * @return a path to the required file
+         *         OR an empty string if either 'dataDirectory' or the option
+         *         with the given name does not exist
+         */
         [[nodiscard]] std::string getDataPath(std::string const &name,
-                                              std::string const &dataSeparator = "") const {
+                                              std::string const &dataSubdir = "") const {
             try {
-                return config["dataDirectory"].getString() + dataSeparator + "/" + config[name].getString();
+                return config["dataDirectory"].getString() + dataSubdir + "/" + config[name].getString();
             } catch (std::out_of_range &e) {
                 spdlog::error("Cannot get a data entry with the name '{}' from '{}' in the config!",
-                              name, dataSeparator);
+                              name, dataSubdir);
                 return "";
             }
         }
 
+        /**
+         * @returns a path to a model file
+         * @see getDataPath
+         */
         [[nodiscard]] std::string getModelPath(std::string const &name,
                                                std::string const &dataSeparator = "") const {
             return getDataPath(name, "/models" + dataSeparator);
