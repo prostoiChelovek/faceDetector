946a9f1f38e9b18fa68882d29d195928855913e3
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2020-09-13 16:28:43 +0500

    add comments to the CentroidTracker and fix non-matched points handling

--- src/Tracker/Implementations/CentroidTracker.cpp
+++ src/Tracker/Implementations/CentroidTracker.cpp
@@ -27,21 +27,29 @@ namespace faces {
                                                              cv::Mat const &prevImg,
                                                              cv::Mat const &actualImg) {
         std::vector<std::pair<int, int>> res;
+
+        // index of the point from actualCentroids: {index of the nearest point from prevCentroids,
+        //                                           distance between that points}
         std::map<std::size_t, std::pair<std::size_t, double>> candidates;
 
         std::vector<cv::Point> prevCentroids = _getCentroids(prevFaces);
         std::vector<cv::Point> actualCentroids = _getCentroids(actualFaces);
 
+        // generating candidate points from prevCentroids for the points from actualCentroids
         for (std::size_t i = 0; i < prevCentroids.size(); ++i) {
+            // distances from the current point (from prevCentroids) to all of the points from actualCentroids
             std::vector<double> distances;
             std::transform(actualCentroids.begin(), actualCentroids.end(), std::back_inserter(distances),
                            [&](cv::Point &pt) -> double { return getDist(prevCentroids[i], pt); });
 
+            // smallest distance to the point from actualCentroids
             auto minDistance = std::min_element(distances.begin(), distances.end());
-            if (!distances.empty()) {
+            if (!distances.empty()) { // is there are any points in actualCentroids -> is *minDistance valid
                 int idx = std::distance(distances.begin(), minDistance);
 
                 decltype(candidates)::iterator candidateItr;
+                // is there a already candidate for the found nearest point
+                // and the distance to the current point is smaller than candidate's
                 bool isFoundAndGreater = (candidateItr = candidates.find(idx)) != candidates.end()
                                          && *minDistance < candidateItr->second.second;
                 if (*minDistance < _maxDistance && (isFoundAndGreater || candidateItr == candidates.end())) {
@@ -52,23 +60,26 @@ namespace faces {
             }
         }
 
+        // adding best candidates to the resulting vector
         for (auto const &candidate : candidates) {
             std::size_t const &actualIdx = candidate.first;
             std::size_t const &prevIdx = candidate.second.first;
             res.emplace_back(prevIdx, actualIdx);
 
+            // marking points as matched
             prevCentroids[prevIdx].x = -1;
             actualCentroids[actualIdx].x = -1;
         }
 
+        // checking non-matched points
         for (std::size_t i = 0; i < prevCentroids.size(); ++i) {
             if (prevCentroids[i].x != -1) {
-                res.emplace_back(-1, i);
+                res.emplace_back(i, -1);
             }
         }
         for (std::size_t i = 0; i < actualCentroids.size(); ++i) {
             if (actualCentroids[i].x != -1) {
-                res.emplace_back(i, -1);
+                res.emplace_back(-1, i);
             }
         }
 
